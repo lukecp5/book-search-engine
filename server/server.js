8dc6e6efb1d92890fd4240d27cee5c1374eb02f9
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-14 18:54:44 -0500

    Add path to buildpath, set default location, port, and connect to database

--- server/server.js
+++ server/server.js
@@ -20,6 +20,20 @@ app.use(express.json());
 
 app.use('/', routes); // + this is the RESTful path for the routes file
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+
+db.connect();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
