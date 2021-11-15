3324dc43d236c10cc95c7b706bafe5611a73da32
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-15 15:57:50 -0500

    Add 'me' Query that will find a user if one is present in the context provided by the authMiddleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,16 @@ const { User } = require('../models');
 
 const resolvers = {
 
-
+// > Query that will find a user(if one is present) in the context provided by the authMiddleware, and return the user.
       Query: {
+            me: async (parent, args, context) => {
+                  if (context.user) {
+                    return User.findOne({ _id: context.user._id });
+                  }
+                  throw new AuthenticationError('You need to be logged in!');
+                },
+      },
+
 
       },
 
