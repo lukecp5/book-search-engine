f1b58058e80716df3774d4cb725a8282a24dca86
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-16 12:17:18 -0500

    Instantiate ApolloClient and pass it the auth middleware and the cache

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,12 @@ const authLink = setContext((_, { headers }) => {
 	};
 });
 
+const client = new ApolloClient({
+	// > Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <Router>
