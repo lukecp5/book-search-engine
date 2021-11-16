df6648a2c5b04548cc8754d3686d579ebfc54a3c
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-16 11:27:32 -0500

    Create REMOVE_BOOK mutation that will be used in conjunction with the useMutation hook to remove books from the database

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -41,3 +41,25 @@ import { gql } from "@apollo/client";
 		}
 	`,
 
+      export const REMOVE_BOOK = gql`
+            mutation removeBook($bookId: ID!) {
+                  removeBook(bookId: $bookId) {
+                        _id
+                        username
+                        email
+                        savedBooks {
+                              bookId
+                              authors
+                              image
+                              description
+                              title
+                              link
+                        }
+                  }
+            }
+      `,
+
+
+
+
+
