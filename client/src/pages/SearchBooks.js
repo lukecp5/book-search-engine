63dbd09591dfffd714ada2783159db7dbe320bc3
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-16 13:03:20 -0500

    Create an await call of the saveBook mutation function containing the bookData of the bookId that was sent from the component when the user clicks the save button

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -27,63 +27,65 @@ const SearchBooks = () => {
 		return () => saveBookIds(savedBookIds);
 	});
 
-  // > create method to search for books and set state on form submit
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+	// > create method to search for books and set state on form submit
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
 
-    if (!searchInput) {
-      return false;
-    }
+		if (!searchInput) {
+			return false;
+		}
 
-    try {
-      const response = await searchGoogleBooks(searchInput);
+		try {
+			const response = await searchGoogleBooks(searchInput);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+			if (!response.ok) {
+				throw new Error("something went wrong!");
+			}
 
-      const { items } = await response.json();
+			const { items } = await response.json();
 
-      const bookData = items.map((book) => ({
-        bookId: book.id,
-        authors: book.volumeInfo.authors || ['No author to display'],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || '',
-      }));
+			const bookData = items.map((book) => ({
+				bookId: book.id,
+				authors: book.volumeInfo.authors || ["No author to display"],
+				title: book.volumeInfo.title,
+				description: book.volumeInfo.description,
+				image: book.volumeInfo.imageLinks?.thumbnail || "",
+			}));
 
-      setSearchedBooks(bookData);
-      setSearchInput('');
-    } catch (err) {
-      console.error(err);
-    }
-  };
+			setSearchedBooks(bookData);
+			setSearchInput("");
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  //    create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
-    // > find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+	// > Create function to handle saving a book to our database and update savedBookIds state on success
+	const handleSaveBook = async (bookId) => {
+		// > find the book in `searchedBooks` state by the matching id
+		const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // > get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+		// > Get the JWT token from context
+		const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+		// > If the token is missing, the user is not logged in and we can't save the book
+		if (!token) {
+			return false;
+		}
 
-    try {
-      const response = await saveBook(bookToSave, token);
+		try {
+			// > Call SAVE_BOOK mutation using our saveBook mutation function created when we passed our parsed GraphQL query to useMutation
+			const { data } = await saveBook({
+				variables: { bookData: { ...bookToSave } },
+			});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+			console.log(data);
 
-      // * if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+			// > If book successfully saves to user's account, save book id to state
+			setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
   return (
     <>
