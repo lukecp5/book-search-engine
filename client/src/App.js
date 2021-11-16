0545f3b205f495f69b6efb871605cf5d400a7e46
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-16 12:13:33 -0500

    Import necessary packages to make requests work with the Apollo Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 
 function App() {
   return (
