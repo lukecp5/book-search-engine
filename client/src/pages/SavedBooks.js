a2e24bd670cb6ab67979e97166b2b4e48cba91de
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-18 13:32:58 -0500

    Fix import locations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Jumbotron, Container, CardColumns, Card, Button } from "react-bootstrap";
 
 // import { getMe, deleteBook } from "../utils/API";
@@ -6,7 +6,8 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const { useQuery, useMutation } = require("@apollo/client");
-const { QUERY_ME, REMOVE_BOOK } = require("../utils/queries");
+const { QUERY_ME } = require("../utils/queries");
+const { REMOVE_BOOK } = require("../utils/mutations");
 
 /*
 
