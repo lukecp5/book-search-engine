fd8d6a4c6e8e8290bc6ca5979643dd6ecac789f7
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-18 13:34:38 -0500

    Remove the error destructuring from the saveBook mutation so the error can be used somewhere else

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -19,7 +19,7 @@ const SearchBooks = () => {
 	const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
 	// > create state to hold the status of our saveBook mutation
-	const [saveBook, { error }] = useMutation(SAVE_BOOK);
+	const [saveBook] = useMutation(SAVE_BOOK);
 
 	// > set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
 	// > learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
