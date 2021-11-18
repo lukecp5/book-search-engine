30653e1a03f31a63824357934c18a7f74485d9e6
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-18 13:34:04 -0500

    Add import for useMutation. Add searchGoogleBooks from the client-side API back into the app

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,9 +3,9 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from "reac
 
 // > Import SAVE_BOOK mutation from utils/mutations.js
 import { SAVE_BOOK } from "../utils/mutations";
-
+import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
-// import { saveBook, searchGoogleBooks } from "../utils/API";
+import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 const SearchBooks = () => {
