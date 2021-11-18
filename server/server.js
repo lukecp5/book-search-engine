f7c288ba2b42e68a1368a4a8008830021db29fe0
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-18 15:14:43 -0500

    Update server

--- server/server.js
+++ server/server.js
@@ -29,14 +29,14 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 // app.use(routes);
 
